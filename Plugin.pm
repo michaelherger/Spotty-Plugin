d62871b18f6620079c0186e0a07809a37c18ea9d
Michael Herger <michael@herger.net>
2017-05-12 15:58:36 +0000

    Add experimental support for Spotify Connect - spotty application would use additional script to have LMS flush buffers etc. - librespot currently has a bug causing a track to end early and skip to the next - transcoding currently is disabled, as it accentuates the aforementioned buffering issue - add binary for arm-linux
    git-svn-id: file:///home/e-smith/files/ibays/slim/svn/trunk/Plugins/Spotty@2158 fbb03dc3-500d-0410-bfac-e8acfcf77fba

--- Plugin.pm
+++ Plugin.pm
@@ -32,11 +32,44 @@ sub initPlugin {
 	$VERSION = $class->pluginDataFor('version');
 	Slim::Player::ProtocolHandlers->registerHandler('spotty', 'Plugins::Spotty::ProtocolHandler');
 
+#                                                                |requires Client
+#                                                                |  |is a Query
+#                                                                |  |  |has Tags
+#                                                                |  |  |  |Function to call
+#                                                                C  Q  T  F
+	Slim::Control::Request::addDispatch(['spotty'],
+	                                                            [1, 0, 0, sub {
+																	my $request = shift;
+																	my $client = $request->client();
+																	
+																	# check buffer usage - no need to skip if buffer is empty
+																	my $usage = $client->usage;
+													
+																	if ( $usage && $client->can('skipAhead') ) {
+																		Slim::Utils::Timers::killTimers($client, \&_skipAhead);
+																		Slim::Utils::Timers::setHighTimer($client, Time::HiRes::time() + 1, \&_skipAhead, $usage);
+																	}
+
+																	$request->setStatusDone();
+	                                                            }]);
+
 	if (main::WEBUI) {
 		require Plugins::Spotty::Settings;
 		require Plugins::Spotty::SettingsAuth;
 		Plugins::Spotty::Settings->new();
 	}
+}
+
+sub _skipAhead {
+	my ($client, $usage) = @_;
+	$client->execute(["mixer", "muting", 1]);
+	my $bitrate = $client->streamingSong()->streambitrate();
+	#my $bufferSize = $client->bufferSize;		# bytes?
+	my $delta = $client->bufferSize * 8 / $bitrate * $usage * 2;
+																		
+#	warn Data::Dump::dump($bitrate, $client->bufferSize, $usage, $delta);
+	$client->skipAhead($delta);
+	$client->execute(["mixer", "muting", 0]);
 }
 
 sub postinitPlugin {
@@ -47,10 +80,17 @@ sub postinitPlugin {
 
 	# modify the transcoding helper table to inject our cache folder
 	my $cacheDir = $class->cacheFolder();
+	my $flushBuffer = Slim::Utils::Misc::findbin('flushbuffers') || '';
+	my $serverPort = preferences('server')->get('httpport');
 
 	foreach ( keys %Slim::Player::TranscodingHelper::commandTable ) {
-		if ($_ =~ /^spt-/ && $Slim::Player::TranscodingHelper::commandTable{$_} =~ /single-track/) {
+		if ( $_ =~ /^spt-/ && $Slim::Player::TranscodingHelper::commandTable{$_} =~ /single-track/ ) {
 			$Slim::Player::TranscodingHelper::commandTable{$_} =~ s/\$CACHE\$/$cacheDir/g;
+		}
+
+		if ( $flushBuffer && $_ =~ /^sptc-/ ) {
+			$Slim::Player::TranscodingHelper::commandTable{$_} =~ s/\$FLUSHBUFFERS\$/$flushBuffer/g;
+			$Slim::Player::TranscodingHelper::commandTable{$_} =~ s/\$SERVERPORT\$/$serverPort/g;
 		}
 	}
 }
