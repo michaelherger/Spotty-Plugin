e019220573c98cfebfadd6d44f7b1220359cea87
Michael Herger <michael@herger.net>
2017-06-26 03:51:01 +0000

    Enable browsing everywhere
    git-svn-id: file:///home/e-smith/files/ibays/slim/svn/trunk/Plugins/Spotty@2229 fbb03dc3-500d-0410-bfac-e8acfcf77fba

--- Plugin.pm
+++ Plugin.pm
@@ -83,7 +83,7 @@ sub initPlugin {
 		Plugins::Spotty::Settings->new();
 	}
 	
-	if ( $prefs->get('enableBrowseMode') ) {
+#	if ( $prefs->get('enableBrowseMode') ) {
 		$class->SUPER::initPlugin(
 			feed   => \&Plugins::Spotty::OPML::handleFeed,
 			tag    => 'spotty',
@@ -93,7 +93,7 @@ sub initPlugin {
 		);
 		
 		Plugins::Spotty::OPML->init();
-	}
+#	}
 
 	if ( Slim::Utils::Versions->compareVersions($::VERSION, '7.9.1') < 0 ) {
 		$log->error('Please update to Logitech Media Server 7.9.1 if you want to use seeking in Spotify tracks.');
--- Settings.pm
+++ Settings.pm
@@ -34,7 +34,7 @@ sub page {
 }
 
 sub prefs {
-	return ($prefs, qw(enableBrowseMode myAlbumsOnly audioCacheSize iconCode));
+	return ($prefs, qw(myAlbumsOnly audioCacheSize iconCode));
 }
 
 sub handler {
