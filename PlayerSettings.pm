c1cf994bc191c17b74639637eab3b3c6e81f8898
Michael Herger <michael@herger.net>
2017-09-21 19:47:26 +0000

    Add a few files I missed...
    git-svn-id: file:///home/e-smith/files/ibays/slim/svn/trunk/Plugins/Spotty@2299 fbb03dc3-500d-0410-bfac-e8acfcf77fba

---
+++ PlayerSettings.pm
@@ -0,0 +1,34 @@
+package Plugins::Spotty::PlayerSettings;
+
+# Logitech Media Server Copyright 2001-2011 Logitech.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+
+my $prefs = preferences('plugin.spotty');
+my $log   = logger('plugin.spotty');
+
+sub name {
+	return Slim::Web::HTTP::CSRF->protectName('PLUGIN_SPOTTY');
+}
+
+sub needsClient {
+	return 1;
+}
+
+sub page {
+	return Slim::Web::HTTP::CSRF->protectURI('plugins/Spotty/settings/player.html');
+}
+
+sub prefs {
+	my ($class, $client) = @_;
+	return ($prefs->client($client), qw(enableSpotifyConnect));
+}
+
+1;
