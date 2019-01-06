2411f3399368e1e4bdd255a4a461a8449aff79fa
Michael Herger <michael@herger.net>
2019-01-06 21:03:08 +0100

    Add findSpotifyFolderFiles helper for Windows

--- Bin/findSpotifyFolderFiles.sh
+++ Bin/findSpotifyFolderFiles.sh
@@ -2,7 +2,7 @@
 
 echo Finding files with playlist folder information:
 
-for FOLDER in "$HOME/.cache/spotify/Storage" "$HOME/Library/Application Support/Spotify/PersistentCache/Storage" "$XDG_CACHE_HOME/spotify/Storage"
+for FOLDER in "$HOME/.cache/spotify/Storage" "$HOME/Library/Application Support/Spotify/PersistentCache/Storage" "$XDG_CACHE_HOME/spotify/Storage" "$LOCALAPPDATA/Spotify/Storage"
 do
    if [ -z "$1" ]; then
       grep -rls "start-group" "$FOLDER"
